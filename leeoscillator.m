ac354aeab86b7b234498b4da953834cb90e8d85e
Bowen WU <bowenbhwu@gmail.com>
2021-06-21 11:20:22 +0800

    Added: Lee Oscillator

---
+++ leeoscillator.m
@@ -0,0 +1,16 @@
+function z = leeoscillator(x)
+load('leeoscillator.mat');
+z = x(:);
+m = size(Z, 2);
+n = length(z);
+for i = 1:n
+    if z(i) < -1 || z(i) > 1
+        z(i) = sigmoid(z(i));
+    else
+        row = int32((z(i)+1)/stepsize) + 1;
+        col = int32(rand() * (m - 1)) + 1;
+        z(i) = z(i) * Z(row, col);
+    end
+end
+z = reshape(z, size(x));
+end
